212c7bfc863b43fd84353c787029e9a5cebf0967
lmzintgraf <lmzintgraf@gmail.com>
2017-06-02 15:25:16 +0100

    add migration behaviour

--- simulator/transaction_model.py
+++ simulator/transaction_model.py
@@ -10,6 +10,7 @@ from simulator.customer import Customer
 from simulator.fraudster import Fraudster
 from simulator.merchant import Merchant
 from simulator.log_collector import LogCollector
+from simulator import parameters
 
 
 class TransactionModel(Model):
@@ -21,6 +22,7 @@ class TransactionModel(Model):
         self.parameters = model_parameters
         self.random_state = np.random.RandomState(self.parameters["seed"])
         self.curr_datetime = self.parameters['start date']
+        self.curr_month = self.curr_datetime.month
         self.terminated = False
 
         # create the payment processing platform via which all transactions go
@@ -28,8 +30,14 @@ class TransactionModel(Model):
 
         # create merchants, customers and fraudsters
         self.merchants = self.instantiate_merchants()
+
         self.customers = [Customer(i, self) for i in range(self.parameters["start num customers"])]
+        self.customer_count = len(self.customers)
+        self.customers_that_left = 0
+        self.customers_that_came = 0
+
         self.fraudsters = [Fraudster(i, self) for i in range(self.parameters["start num fraudsters"])]
+        self.fraudster_count = len(self.fraudsters)
 
         # TODO: social network between customers
         # grid = Grid(10, 10, 5)
@@ -38,13 +46,11 @@ class TransactionModel(Model):
 
         # set up a schedule
         self.schedule = RandomActivation(self)
-        a = [self.schedule.add(self.customers[i]) for i in range(len(self.customers))]
-        a = [self.schedule.add(self.fraudsters[i]) for i in range(len(self.fraudsters))]
 
         # create data collector for the transaction logs
         self.log_collector = LogCollector(
             agent_reporters={"Date": lambda c: c.model.curr_datetime,
-                             "CardID": lambda c: c.unique_id,
+                             "CardID": lambda c: c.card,
                              "MerchantID": lambda c: c.curr_merchant.unique_id,
                              "Amount": lambda c: c.curr_transaction_amount,
                              "Currency": lambda c: c.currency,
@@ -53,7 +59,17 @@ class TransactionModel(Model):
 
     def step(self):
 
+        # print updates
+        if self.curr_datetime.month != self.curr_month:
+            self.curr_month = self.curr_datetime.month
+            print("month", self.curr_month)
+            print("num customers", len(self.customers))
+            print("num fraudsters", len(self.fraudsters))
+
         # this calls the step function of each agent in the schedule (customer, fraudster)
+        self.schedule.agents = []
+        self.schedule.agents.extend(self.customers)
+        self.schedule.agents.extend(self.fraudsters)
         self.schedule.step()
 
         # write new transactions to log
@@ -66,7 +82,31 @@ class TransactionModel(Model):
         if self.curr_datetime.date() > self.parameters['end date'].date():
             self.terminated = True
 
-        # TODO: customer/fraudster migration
+        self.migration()
+
+    def migration(self):
+
+        # LEAVING customers/fraudsters
+        self.customers = [c for c in self.customers if c.stay]
+        self.fraudsters = [f for f in self.fraudsters if f.stay]
+
+        # COMING customers/fraudsters
+        leave_prob = 1 - self.parameters['stay_prob']
+
+        # the number of customers making a transaction at each step
+        avg_num_cust = int(parameters.aggregated_data.loc['transactions']['non-fraud']) / 366. / 24.
+        avg_cust_leave = avg_num_cust * leave_prob[0]
+        avg_cust_leave += self.random_state.normal(0, 1, 1)[0]
+        for i in range(int(np.round(avg_cust_leave,0))):
+            self.customers.append(Customer(self.customer_count, self))
+            self.customer_count += 1
+
+        # the number of fraudsters making a transaction at each step
+        avg_num_fraud = int(parameters.aggregated_data.loc['transactions']['fraud'])/366./24.
+        avg_fraud_leave = avg_num_fraud * leave_prob[1]
+        if avg_fraud_leave > self.random_state.uniform(0, 1, 1)[0]:
+            self.fraudsters.append(Fraudster(self.fraudster_count, self))
+            self.fraudster_count += 1
 
     def process_transaction(self, client, amount, merchant):
         authorise = self.authenticator.authorise_payment(client, amount, merchant)
