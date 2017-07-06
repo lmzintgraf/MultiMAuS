2887ed302c487c149a854fa406a087842fa84f69
lmzintgraf <lmzintgraf@gmail.com>
2017-07-06 08:14:00 -0400

    adapted simple authenticatiors

--- authenticators/simple_authenticators.py
+++ authenticators/simple_authenticators.py
@@ -3,59 +3,55 @@ from authenticators.abstract_authenticator import AbstractAuthenticator
 
 class OracleAuthenticator(AbstractAuthenticator):
 
-    def authorise_transaction(self, model, customer):
+    def authorise_transaction(self, customer):
         if customer.fraudster:
             return False
         else:
             return True
 
 
-class PositiveAuthenticator(AbstractAuthenticator):
+class NeverSecondAuthenticator(AbstractAuthenticator):
 
-    def authorise_transaction(self, model, customer):
+    def authorise_transaction(self, customer):
         return True
 
 
-class HeuristicAuthenticator(AbstractAuthenticator):
+class AlwaysSecondAuthenticator(AbstractAuthenticator):
 
-    def authorise_transaction(self, model, customer):
-
-        authorise = False
-
-        if customer.curr_amount > 500:
-            for a in range(model.parameters['num_auth_steps']):
-                auth_quality = customer.get_authentication()
-                if auth_quality is None:
-                    authorise = False
-                    break
-                # decide if good enough or else continue
-                elif auth_quality > model.random_state.uniform(0, 1, 1)[0]:
-                    authorise = True
-                    break
+    def authorise_transaction(self, customer):
+        if customer.get_authentication() is not None:
+            return True
         else:
-            authorise = True
+            return False
+
+
+class HeuristicAuthenticator(AbstractAuthenticator):
+    def __init__(self, thresh):
+        self.thresh = thresh
+
+    def authorise_transaction(self, customer):
+
+        authorise = True
+        if customer.curr_amount > self.thresh:
+            auth_quality = customer.get_authentication()
+            if auth_quality is None:
+                authorise = False
 
         return authorise
 
 
 class RandomAuthenticator(AbstractAuthenticator):
 
-    def authorise_transaction(self, model, customer):
+    def authorise_transaction(self, customer):
 
-        authorise = False
-
-        for a in range(model.parameters['num_auth_steps']):
-            # ask for authentication in 50% of the cases
-            if model.random_state.uniform(0, 1, 1)[0] < 0.5:
-                auth_quality = customer.get_authentication()
-                if auth_quality is None:
-                    authorise = False
-                    break
-                # decide if good enough or else continue
-                elif auth_quality > model.random_state.uniform(0, 1, 1)[0]:
-                    authorise = True
-                    break
+        # ask for second authentication in 50% of the cases
+        if customer.model.random_state.uniform(0, 1, 1)[0] < 0.5:
+            auth_quality = customer.get_authentication()
+            if auth_quality is None:
+                authorise = False
             else:
                 authorise = True
+        else:
+            authorise = True
 
         return authorise
