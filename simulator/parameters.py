f017cf77157e19872096800bef814a3207434ea1
lmzintgraf <lmzintgraf@gmail.com>
2017-07-17 12:05:24 +0200

    final params

--- simulator/parameters.py
+++ simulator/parameters.py
@@ -25,12 +25,12 @@ def get_default_parameters():
         # (note that this doesn't really influence the total amount of transactions;
         #  for that change the probability of making transactions)
         'num_customers': 3333,
-        'num_fraudsters': 44,
+        'num_fraudsters': 55,
 
         # the intrinsic transaction motivation per customer (is proportional to number of customers/fraudsters)
         # we keep this separate because then we can play around with the number of customers/fraudsters,
         # but individual behaviour doesn't change
-        'transaction_motivation': np.array([1./3333, 1./44]),
+        'transaction_motivation': np.array([1./3333, 1./55]),
 
         # the initial satisfaction of customers
         'init_satisfaction': 1.,
