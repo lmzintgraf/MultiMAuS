ff31f0ba25b9c84db2317dd6fc5a79252b3729a0
lmzintgraf <lmzintgraf@gmail.com>
2017-09-13 17:28:22 +0200

    auth dont have a name anymore; they now either ask customer for authentication or do nothing

--- authenticators/abstract_authenticator.py
+++ authenticators/abstract_authenticator.py
@@ -2,13 +2,12 @@ from abc import ABCMeta, abstractmethod
 
 
 class AbstractAuthenticator(metaclass=ABCMeta):
-    def __init__(self, name):
+    def __init__(self):
         """
         Every authenticator has to have a name
         :param name: 
         """
         super().__init__()
-        self.name = name
 
     @abstractmethod
     def authorise_transaction(self, customer):
@@ -17,14 +16,4 @@ class AbstractAuthenticator(metaclass=ABCMeta):
         Note that all relevant information can be obtained from the customer.
         :param customer:    the customer making a transaction
         :return:            boolean, whether or not to authorise the transaction
-        """
-
-
-class AbstractRLAuthenticator(AbstractAuthenticator, metaclass=ABCMeta):
-
-    @abstractmethod
-    def receive_reward(self):
-        """
-        After each transaction, get a reward signal from the environment.
-        :return: 
         """
--- authenticators/simple_authenticators.py
+++ authenticators/simple_authenticators.py
@@ -2,65 +2,34 @@ from authenticators.abstract_authenticator import AbstractAuthenticator
 
 
 class OracleAuthenticator(AbstractAuthenticator):
-    def __init__(self):
-        super().__init__(name='oracle')
-
     def authorise_transaction(self, customer):
         if customer.fraudster:
-            return False
-        else:
-            return True
+            customer.give_authentication()
 
 
 class NeverSecondAuthenticator(AbstractAuthenticator):
-    def __init__(self):
-        super().__init__(name='never_second')
-
     def authorise_transaction(self, customer):
-        return True
+        pass
 
 
 class AlwaysSecondAuthenticator(AbstractAuthenticator):
-    def __init__(self):
-        super().__init__(name='always_second')
-
     def authorise_transaction(self, customer):
-        if customer.give_authentication() is not None:
-            return True
-        else:
-            return False
+        customer.give_authentication()
 
 
 class HeuristicAuthenticator(AbstractAuthenticator):
     def __init__(self, thresh=50):
-        super().__init__(name='heuristic')
+        super().__init__()
         self.thresh = thresh
 
     def authorise_transaction(self, customer):
-
-        authorise = True
         if customer.curr_amount > self.thresh:
-            auth_quality = customer.give_authentication()
-            if auth_quality is None:
-                authorise = False
-
-        return authorise
+            customer.give_authentication()
 
 
 class RandomAuthenticator(AbstractAuthenticator):
-    def __init__(self):
-        super().__init__(name='random')
-
     def authorise_transaction(self, customer):
 
         # ask for second authentication in 50% of the cases
         if customer.model.random_state.uniform(0, 1, 1)[0] < 0.5:
-            auth_quality = customer.give_authentication()
-            if auth_quality is None:
-                authorise = False
-            else:
-                authorise = True
-        else:
-            authorise = True
-
-        return authorise
+            customer.give_authentication()
