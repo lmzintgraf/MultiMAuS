4e74982eaeae427338e1d69749409e51839fdd92
lmzintgraf <lmzintgraf@gmail.com>
2017-11-01 12:01:05 +0000

    added currencies (commented out0

--- learning/state_space.py
+++ learning/state_space.py
@@ -6,11 +6,14 @@ meaning it should include all relevant information that an agent could use
 when making a decision; and it should not include any unnecessary information.
 """
 
+import numpy as np
+
 # the number of different amount categories we use
 NUM_AMOUNT_CAT = 6
+NUM_CURRENCY_CAT = 4
 
 # the actual size of the state space; the product of the above
-SIZE = NUM_AMOUNT_CAT
+SIZE = NUM_AMOUNT_CAT * NUM_CURRENCY_CAT
 
 
 def get_state(customer):
@@ -21,6 +24,11 @@ def get_state(customer):
     :return:
     """
     amount_cat = get_amount_category(customer.curr_amount)
+    # currency_cat = get_currency_category(customer.currency)
+
+    # indices = np.array(range(SIZE)).reshape((NUM_AMOUNT_CAT, NUM_CURRENCY_CAT))
+    # state_index = indices[amount_cat, currency_cat]
+
     return amount_cat
 
 
@@ -38,3 +46,15 @@ def get_amount_category(amount):
     else:
         amount_category = 5
     return amount_category
+
+
+def get_currency_category(currency):
+    if currency == 'EUR':
+        currency_category = 0
+    elif currency == 'USD':
+        currency_category = 1
+    # elif currency == 'GBP':
+    #     currency_category = 2
+    else:
+        currency_category = 2
+    return currency_category
